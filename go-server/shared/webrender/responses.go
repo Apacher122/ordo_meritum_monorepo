641aed3ac83595134eda8a7ac92e232b374a75c4
ApacheR122 <ApareceR12@gmail.com>
2025-10-27 23:56:50 -0500

    adding more documentation improvements

--- go-server/shared/webrender/responses.go
+++ go-server/shared/webrender/responses.go
@@ -10,6 +10,13 @@ import (
 	error_response "github.com/ordo_meritum/shared/types/errors"
 )
 
+// DecodeJSONBody is a generic helper function that decodes a JSON request body
+// into a provided destination variable. If decoding fails, it logs the error,
+// writes a standardized error response to the http.ResponseWriter with a 400 Bad Request status,
+// and returns the underlying decoding error.
+//
+// The destination variable 'v' should be a pointer to the target struct.
+// It returns nil on successful decoding.
 func DecodeJSONBody[T any](w http.ResponseWriter, r *http.Request, v T) error {
 	err := json.NewDecoder(r.Body).Decode(v)
 
