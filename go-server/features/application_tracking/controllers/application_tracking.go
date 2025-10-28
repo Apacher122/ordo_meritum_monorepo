da37570f41bbac17f5569facca5f5e76c6beab1f
ApacheR122 <ApareceR12@gmail.com>
2025-10-27 23:17:15 -0500

    added missing defer

--- go-server/features/application_tracking/controllers/application_tracking.go
+++ go-server/features/application_tracking/controllers/application_tracking.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"strconv"
@@ -10,9 +11,13 @@ import (
 	"github.com/ordo_meritum/features/application_tracking/services"
 	"github.com/ordo_meritum/shared/contexts"
 	"github.com/ordo_meritum/shared/middleware"
+	error_messages "github.com/ordo_meritum/shared/utils/errors"
+	lg "github.com/ordo_meritum/shared/utils/logger"
 	"github.com/ordo_meritum/shared/webrender"
 	"github.com/rs/zerolog/log"
 )
+
+var service = "app-tracker-controller"
 
 type Controller struct {
 	service *services.AppTrackerService
@@ -23,10 +28,10 @@ func NewController(service *services.AppTrackerService) *Controller {
 }
 
 func (c *Controller) RegisterRoutes(secureRouter *mux.Router, authRouter *mux.Router) {
-	secureRouter.HandleFunc("/apps/track", c.HandleTrackApplication).Methods("POST")
-	authRouter.HandleFunc("/apps/track/list", c.HandleListApplications).Methods("GET")
-	authRouter.HandleFunc("/apps/update", c.HandleUpdateApplication).Methods("PATCH")
-	authRouter.HandleFunc("/track/{id:[0-9]+}", c.HandleGetTrackedApplication).Methods("GET")
+	secureRouter.HandleFunc("/apps/track", c.trackNewApplicationHandler).Methods("POST")
+	authRouter.HandleFunc("/apps/track/list", c.getApplicationListHandler).Methods("GET")
+	authRouter.HandleFunc("/apps/update", c.updateApplicationHandler).Methods("PATCH")
+	authRouter.HandleFunc("/track/{id:[0-9]+}", c.getApplicationByIDHandler).Methods("GET")
 }
 
 func parseIDFromVars(r *http.Request) (int, error) {
@@ -34,40 +39,50 @@ func parseIDFromVars(r *http.Request) (int, error) {
 	return strconv.Atoi(idStr)
 }
 
-// HandleTrackApplication queues a job for tracking based on the request body and returns the job ID of the queued job.
-// It takes in a request body and returns a job ID of the queued job and an error if any errors occur.
-// If the user context is not found, it will return an error with ErrorCode set to ERR_USER_NO_CONTEXT.
-// If the request body is malformed, it will return an error with ErrorCode set to ERR_INVALID_REQUEST_BODY.
-// If the job cannot be queued, it will return an error with ErrorCode set to ERR_FAILED_TO_QUEUE_JOB.
-// It will log a message with the jobID and the status of the job.
-func (c *Controller) HandleTrackApplication(w http.ResponseWriter, r *http.Request) {
+// trackNewApplicationHandler is an HTTP handler that creates a new job posting for a given request body.
+// It requires the user context to be present in the request context.
+// If the request body is malformed, it will return an HTTP 400 status with an error message.
+// If the user context is not found, it will return an HTTP 500 status with an error message.
+// If the job posting is successfully created, it will return an HTTP 202 status with a message indicating that the job posting was received and is being processed.
+// The actual creation of the job posting is done in the background using a goroutine.
+func (c *Controller) trackNewApplicationHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	_, ok := contexts.FromContext(r.Context())
+	userCtx, ok := contexts.FromContext(r.Context())
 	if !ok {
+		lg.ErrorLoggerType{Service: &service, ErrorCode: &error_messages.ERR_USER_NO_CONTEXT}.ErrorLog()
 		middleware.JSON(w, http.StatusInternalServerError, nil)
 		return
 	}
+
 	var requestBody request.JobPostingRequest
 	if webrender.DecodeJSONBody(w, r, &requestBody) != nil {
 		return
 	}
+	middleware.JSON(w, http.StatusAccepted, map[string]string{"status": "Job posting received and is being processed"})
+	go func() {
+		bgCtx := context.Background()
+		bgUserCtx := &contexts.UserContext{
+			UID:    userCtx.UID,
+			ApiKey: userCtx.ApiKey,
+		}
+		ctxWithUser := context.WithValue(bgCtx, contexts.UserContextKey, bgUserCtx)
 
-	jobID, err := c.service.QueueApplicationTracking(r.Context(), requestBody)
-	if err != nil {
-		middleware.JSON(w, http.StatusInternalServerError, nil)
-		return
-	}
-
-	middleware.JSON(w, http.StatusCreated, jobID)
+		jobID, err := c.service.QueueApplicationTracking(ctxWithUser, &requestBody)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create job posting in background")
+			return
+		}
+		log.Info().Int("jobID", jobID).Msg("Successfully created job posting in background")
+	}()
 }
 
-// HandleListapplications returns a list of all tracked applications for the user.
+// getApplicationListHandler retrieves a list of all tracked job postings for a given user.
 // It requires the user context to be present in the request context.
-// If the user context is not found, it will return an error with ErrorCode set to ERR_USER_NO_CONTEXT.
-// If the applications cannot be retrieved, it will return an error with ErrorCode set to ERR_FAILED_TO_RETRIEVE_APPLICATIONS.
-// It will log a message with the applications and the status of the request.
-func (c *Controller) HandleListApplications(w http.ResponseWriter, r *http.Request) {
+// If the user context is not found, it will return an HTTP 500 status with an error message.
+// If the job postings cannot be retrieved, it will return an HTTP 500 status with an error message.
+// It will log a message with the job postings and the status of the request.
+func (c *Controller) getApplicationListHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	_, ok := contexts.FromContext(r.Context())
@@ -84,13 +99,12 @@ func (c *Controller) HandleListApplications(w http.ResponseWriter, r *http.Reque
 	middleware.JSON(w, http.StatusOK, applications)
 }
 
-// HandleGetTrackedApplication returns a single tracked application by ID.
-// It requires the user context to be present in the request context and the roleID to be present in the request variables.
-// If the user context is not found, it will return an error with ErrorCode set to ERR_USER_NO_CONTEXT.
-// If the roleID is not present or cannot be parsed, it will return an error with ErrorCode set to ERR_INVALID_REQUEST_BODY.
-// If the application cannot be retrieved, it will return an error with ErrorCode set to ERR_FAILED_TO_RETRIEVE_APPLICATION.
-// It will log a message with the application and the status of the request.
-func (c *Controller) HandleGetTrackedApplication(w http.ResponseWriter, r *http.Request) {
+// getApplicationByIDHandler retrieves a tracked job posting by ID.
+// It requires the user context to be present in the request context.
+// If the user context is not found, it will return an HTTP 500 status with an error message.
+// If the job posting cannot be found, it will return an HTTP 404 status with an error message.
+// It will log a message with the job posting and the status of the request.
+func (c *Controller) getApplicationByIDHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	roleID, err := parseIDFromVars(r)
@@ -108,12 +122,14 @@ func (c *Controller) HandleGetTrackedApplication(w http.ResponseWriter, r *http.
 	middleware.JSON(w, http.StatusOK, application)
 }
 
-// HandleUpdateApplication updates the status of an application based on the request body.
-// It requires the user context to be present in the request context.
-// If the request body is malformed, it will return an error with ErrorCode set to ERR_INVALID_REQUEST_BODY.
-// If the application status cannot be updated, it will return an error with ErrorCode set to ERR_FAILED_TO_UPDATE_STATUS.
-// It will log a message with the application and the status of the request.
-func (c *Controller) HandleUpdateApplication(w http.ResponseWriter, r *http.Request) {
+// updateApplicationHandler updates the application status for a given job posting.
+// It takes in a request which contains the role ID and the application status.
+// It will return an error if any errors occur.
+// If the role ID cannot be found, it will return an error with ErrorCode set to ERR_DB_FAILED_TO_GET.
+// If the user context is not found, it will return an error with ErrorCode set to ERR_USER_NO_CONTEXT.
+// If the update process fails, it will return an HTTP 500 status with an error message.
+// If the update process is successful, it will return an HTTP 200 status with a message indicating that the status was updated successfully.
+func (c *Controller) updateApplicationHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	_, ok := contexts.FromContext(r.Context())
